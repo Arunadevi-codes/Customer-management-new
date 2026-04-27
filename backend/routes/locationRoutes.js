acde8695798e9c4bdf85e27d7d31349b57e80424
Arunadevi-codes <aruganesan03@gmail.com>
2026-04-27 18:59:47 +0530

    Remove backend and frontend folder from GitHub tracking

--- backend/routes/locationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getCitiesByState, getAllStates } = require("../controllers/locationController");
-
-router.get("/states", getAllStates);
-router.get("/cities", getCitiesByState);
-
-module.exports = router;
--- frontend/src/services/locationServices.js
+++
@@ -1,9 +0,0 @@
-export const fetchStates = async () => {
-  const res = await fetch("http://localhost:5000/api/states");
-  return res.json();
-};
-
-export const fetchCities = async (stateId) => {
-  const res = await fetch(`http://localhost:5000/api/cities?stateId=${stateId}`);
-  return res.json();
-};
